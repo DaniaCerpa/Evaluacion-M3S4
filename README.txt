d9da6102e79ed857de441533b16d12fa207e61ee
DaniaCerpa <dania.cerpa@gmail.com>
2024-08-12 00:09:08 -0400

    Primer commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Alumno: Dania Cerpa
+Curso: BOTIC-SOFOF-23-30-13-0068 -DESARROLLO DE APLICACIONES FULL STACK PYTHON TRAINEE V2.0
+Modulo: 3
+Sesión: 4
+Tarea tipo: Ejercicio de comprobacióm
+Repositorio Github: https://github.com/DaniaCerpa/Ej-comprobacion-M3S4.git
+Clonar repositorio: git clone https://github.com/DaniaCerpa/Ej-comprobacion-M3S4.git
---
+++ cuarto.py
@@ -0,0 +1,14 @@
+#Creacion de variables deccionarios
+
+midiccionario1 = {"nombre": "Dania", "apellido":"Cerpa", "telefono": 955292785}
+print ("midiccionario1 :", midiccionario1)
+midiccionario2 = {"nombre": "Adan", "apellido":"Medina", "telefono": 991417502}
+print ("midiccionario2 :", midiccionario2)
+midiccionario3 = {"nombre": "Sofia", "apellido":"Oliva", "telefono": 992873512}
+print ("midiccionario3 :", midiccionario3)
+
+#Creacion de lista incluyendo diccionarios
+print ()
+milista = [midiccionario1, midiccionario2, midiccionario3]
+print ("milista :", milista)
+
